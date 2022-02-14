23515c22636e08415859447130262c3d33036801
Stephane Massonnet <stf.massonnet@gmail.com>
2022-02-14 17:49:04 +0100

    Added store documentation

--- docs/references/stores.md
+++ docs/references/stores.md
@@ -1,5 +1,24 @@
 # Stores
 
+Stores are used to load and save models state.
+
+## MLModule pre-trained models
+
+MLModule provides model weights for all defined [models](../models/index.md)
+through the [`MLModuleModelStore`][mlmodule.v2.stores.MLModuleModelStore].
+
+::: mlmodule.v2.stores.MLModuleModelStore
+    selection:
+        members:
+            - load
+
 ## Available stores
 
+::: mlmodule.v2.stores.LocalFileModelStore
+
 ## Write your own store
+
+A store should inherit [`AbstractModelStore`][mlmodule.v2.stores.AbstractModelStore]
+and implement the `save` or `load` methods.
+
+::: mlmodule.v2.stores.AbstractModelStore
--- src/mlmodule/v2/stores.py
+++ src/mlmodule/v2/stores.py
@@ -14,21 +14,28 @@ class AbstractModelStore(abc.ABC):
 
     @abc.abstractmethod
     def save(self, model: ModelWithState) -> None:
-        """Saves the model to the binary file handler"""
+        """Saves the model state to the store
+
+        Attributes:
+            model (ModelWithState): Model to save
+        """
 
     @abc.abstractmethod
     def load(self, model: ModelWithState) -> None:
-        """Loads the models weights from the binary file"""
+        """Loads the models weights from the store
+
+        Attributes:
+            model (ModelWithState): Model to update
+        """
 
 
 class MLModuleModelStore(AbstractModelStore):
-    """Default MLModule store with model states stored in a S3 bucket"""
+    """Default MLModule store with pretrained model states"""
 
     def save(self, model: ModelWithState) -> NoReturn:
         raise ValueError("MLModuleStore states are read-only")
 
     def load(self, model: ModelWithState) -> None:
-        """Reads the model weights from LSIR public assets S3"""
         session = boto3.session.Session(
             aws_access_key_id=os.environ.get("MLMODULE_AWS_ACCESS_KEY_ID"),
             aws_secret_access_key=os.environ.get("MLMODULE_AWS_SECRET_ACCESS_KEY"),
@@ -49,12 +56,27 @@ class MLModuleModelStore(AbstractModelStore):
 
 @dataclasses.dataclass
 class LocalFileModelStore(AbstractModelStore):
+    """Local filebased store
+
+    Attributes:
+        filename (str): Path to the file to save model's state
+    """
     filename: str
 
     def save(self, model: ModelWithState) -> None:
+        """Saves the model state to the local file
+
+        Attributes:
+            model (ModelWithState): Model to save
+        """
         with open(self.filename, mode="wb") as f:
             f.write(model.get_state())
 
     def load(self, model: ModelWithState) -> None:
+        """Loads the models weights from the local file
+
+        Attributes:
+            model (ModelWithState): Model to update
+        """
         with open(self.filename, mode="rb") as f:
             model.set_state(f.read())
