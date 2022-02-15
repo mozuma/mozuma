dd09d55e8bba45cc5785deadb39d83fbb7381e76
Stephane Massonnet <stf.massonnet@gmail.com>
2022-02-15 13:35:26 +0100

    Added label set documentation

--- docs/references/labels.md
+++ docs/references/labels.md
@@ -1,2 +1,11 @@
 
 ::: mlmodule.labels.base.LabelSet
+
+## Available label sets
+
+- `mlmodule.labels.imagenet.IMAGENET_LABELS`
+- `mlmodule.labels.places_io.PLACES_IN_OUT_DOOR`
+- `mlmodule.labels.places_io.PLACES_IO_LABELS`
+- `mlmodule.labels.places.PLACES_LABELS`
+- `mlmodule.labels.vinvl_attributes.VINVL_ATTRIBUTE_LABELS`
+- `mlmodule.labels.vinvl.VINVL_LABELS`
--- src/mlmodule/labels/base.py
+++ src/mlmodule/labels/base.py
@@ -2,8 +2,29 @@ import dataclasses
 from typing import List
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class LabelSet:
+    """Label set is an ordered list of labels used for classification tasks
+
+    Attributes:
+        label_set_unique_id (str): Unique identifier for a label set
+        label_list (List[str]): Ordered list of labels
+
+    Example:
+        LabelSet objects are used as classic lists:
+
+        ```python
+        animal_labels = LabelSet(
+            label_set_unique_id="animals",
+            label_list=["cat", "dog"]
+        )
+        print(animal_labels.label_set_unique_id)    # "animals"
+        print(animal_labels[0])     # "cat"
+        print(animal_labels[1])     # "dog"
+        print(len(animal_labels))   # 2
+        ```
+    """
+
     # Must uniquely define this label set
     label_set_unique_id: str
     label_list: List[str]
