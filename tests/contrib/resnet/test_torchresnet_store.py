e554e680824271257ccccf1e883879804738764d
Stephane Massonnet <stf.massonnet@gmail.com>
2022-03-01 15:01:56 +0100

    Updated ResNet base class

--- tests/contrib/resnet/test_torchresnet_store.py
+++ tests/contrib/resnet/test_torchresnet_store.py
@@ -1,11 +1,11 @@
-from mlmodule.contrib.resnet.modules import TorchResNetImageNetModule
+from mlmodule.contrib.resnet.modules import TorchResNetModule
 from mlmodule.contrib.resnet.stores import ResNetTorchVisionStore
 from mlmodule.v2.states import StateKey
 
 
 def test_torchresnet_store_load():
     store = ResNetTorchVisionStore()
-    model = TorchResNetImageNetModule("resnet18")
+    model = TorchResNetModule("resnet18")
 
     # Initial random state
     random_state = model.get_state()
@@ -19,7 +19,7 @@ def test_torchresnet_store_load():
 
 def test_torchresnet_store_get_state_keys():
     store = ResNetTorchVisionStore()
-    model = TorchResNetImageNetModule("resnet18")
+    model = TorchResNetModule("resnet18")
 
     state_keys = store.get_state_keys(model.state_type)
     assert len(state_keys) == 1
